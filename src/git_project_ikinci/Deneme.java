4405edf008a98356cb3fcf7418dd74fdc8031074
esay2561 <kulebiee@gmail.com>
2020-08-09 16:16:01 -0400

    ikinci satir ekledim

--- src/git_project_ikinci/Deneme.java
+++ src/git_project_ikinci/Deneme.java
@@ -3,8 +3,8 @@ package git_project_ikinci;
 public class Deneme {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("ikinci git problem");
+        System.out.println("ikinci satir kodum");
 	}
 
 }
